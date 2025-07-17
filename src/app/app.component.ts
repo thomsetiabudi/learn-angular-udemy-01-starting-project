eda7017cd8e47d96079fba3c0be8da07f1a328df
Thomas Setiabudi <thomsetiabudi@gmail.com>
2025-07-17 20:48:34 +0700

    section 2 - 42 if and else in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { TasksComponent } from "./tasks/tasks.component";
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId: string | null = null;
+  selectedUserId?: string;
 
   getRandomUser() {
     const randomIndex = Math.floor(Math.random() * this.users.length);
